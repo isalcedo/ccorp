d7d794d1819b95eea4621a430113c2ced47637c1
isalcedo <ignacio@isalcedo.com>
2016-10-09 10:17:52 -0400

    END

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,13 +18,15 @@ weatherApp.controller('getWeatherController', function($scope, $http, $filter) {
 			if (typeof zip_code == 'undefined') {
 				zip_code = '';
 			} 
-			$http.get("https://api.openweathermap.org/data/2.5/weather?zip="+ zip_code +","+ country_code.alpha2 +"&units=metric&APPID=aa8521fe973ae615bd42f0b20a0e47c6")
+			angular.element('.load-weather').slideDown();
+			$http.get("http://api.openweathermap.org/data/2.5/weather?zip="+ zip_code +","+ country_code.alpha2 +"&units=metric&APPID=aa8521fe973ae615bd42f0b20a0e47c6")
 			.then(function(response) {
 				$scope.data = response.data;
-				$scope.icon = "https://openweathermap.org/img/w/"+response.data.weather[0].icon+".png";
+				$scope.icon = "http://openweathermap.org/img/w/"+response.data.weather[0].icon+".png";
 
 				$http.get("https://maps.googleapis.com/maps/api/timezone/json?location="+response.data.coord.lat+","+response.data.coord.lon+"&timestamp="+response.data.sys.sunrise+"&key=AIzaSyBOv4sSajL69MzT9ADx1y3-BHkaH7-sI-M")
 				.then(function(Gresponse) {
+					angular.element('.load-weather').slideUp();
 					sunrise = moment(response.data.sys.sunrise*1000);
 					sunset  = moment(response.data.sys.sunset*1000);
 					$scope.sunrise = sunrise.tz(Gresponse.data.timeZoneId).format('ha'); 
@@ -48,16 +50,18 @@ weatherApp.controller('getWeatherController', function($scope, $http, $filter) {
 				angular.element('div.zip_code').slideUp();
 
 				var geoSuccess = function(position) {
+					angular.element('.load-weather').slideDown();
 					startPos = position;
 					lat  = startPos.coords.latitude;
 					lon  = startPos.coords.longitude;
-					$http.get("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric&APPID=aa8521fe973ae615bd42f0b20a0e47c6")
+					$http.get("http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric&APPID=aa8521fe973ae615bd42f0b20a0e47c6")
 					.then(function(response) {
 						$scope.data    = response.data;
-						$scope.icon    = "https://openweathermap.org/img/w/"+response.data.weather[0].icon+".png";
+						$scope.icon    = "http://openweathermap.org/img/w/"+response.data.weather[0].icon+".png";
 
 						$http.get("https://maps.googleapis.com/maps/api/timezone/json?location="+lat+","+lon+"&timestamp="+response.data.sys.sunrise+"&key=AIzaSyBOv4sSajL69MzT9ADx1y3-BHkaH7-sI-M")
 						.then(function(Gresponse) {
+							angular.element('.load-weather').slideUp();
 							sunrise = moment(response.data.sys.sunrise*1000);
 							sunset  = moment(response.data.sys.sunset*1000);
 							$scope.sunrise = sunrise.tz(Gresponse.data.timeZoneId).format('ha'); 
@@ -76,12 +80,15 @@ weatherApp.controller('getWeatherController', function($scope, $http, $filter) {
 				};
 
 				var geoError = function(error) {
-					console.log(error);
+					angular.element('.no-geo').fadeIn();
+					angular.element('div.zip_code').slideDown();
 				}
 
 				navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
 			} else {
 				console.log('Geolocation is not supported.');
+				angular.element('.no-geo').fadeIn();
+				angular.element('div.zip_code').slideDown();
 			}
 
 		}
